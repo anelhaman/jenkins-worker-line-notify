af7163d646e7f72dfb6b37942b9f1210a7fa01ee
rachen <anelhaman@hotmail.com>
2018-05-31 11:38:47 +0700

    Update line-noti-succeed.py

--- line-noti-succeed.py
+++ line-noti-succeed.py
@@ -13,8 +13,8 @@ url = 'https://notify-api.line.me/api/notify'
 token = '5yAHpkG0sZXhtN7sFSlMJ8SZX1zyaF8VlOeO81ZUhNN'
 headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
 
-time = time.strftime("%c") 
-msg = 'Successfully Build https://www.dohome.co.th/ '+ time
+current_time = time.strftime("%c") 
+msg = 'Successfully Build https://www.dohome.co.th/ '+ current_time
 # msg = 'Error Build, Please re-checking again!'
 
 mydata = {
